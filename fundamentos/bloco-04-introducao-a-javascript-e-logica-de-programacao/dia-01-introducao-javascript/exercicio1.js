68bba3ee93fb9f500db2857540e23b7929b1da97
Rejane Raquel <rejane.lima87@gmail.com>
2022-02-18 13:12:08 -0300

    Adiciona  exercicio video variaveis

---
+++ fundamentos/bloco-04-introducao-a-javascript-e-logica-de-programacao/dia-01-introducao-javascript/exercicio1.js
@@ -0,0 +1,14 @@
+/* Crie uma constante chamada myName e atribua a ela o seu nome (Exemplo: Carolina). */
+const myName = "Rejane Raquel";
+/*Crie uma constante chamada birthCity e atribua a ela a sua cidade natal.  */
+const birthCity = "Brasília";
+/* Crie uma variável chamada birthYear e atribua a ela o ano em que você nasceu. */
+var birthYear = 1987;
+/* Utilize o console.log() para imprimir as constantes e variáveis que você criou. */
+console.log(myName, birthCity, birthYear);
+/* Altere o valor atribuído à variável birthYear para 2030. Faça um console.log(birthYear) novamente para ver o que acontece! */
+var birthYear = 2030;
+console.log(birthYear);
+/*  Altere o valor atribuído à constante birthCity . Faça um console.log(birthCity) novamente! Você saberia explicar por que recebemos uma mensagem de erro?*/
+const birthCity = "Natal"; //o novo valor não será atribuído pois trata-se de uma constante!
+
