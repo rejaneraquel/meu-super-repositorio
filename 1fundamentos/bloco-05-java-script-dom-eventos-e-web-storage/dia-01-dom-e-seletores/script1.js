ddbce39022123c52fb607b4f3c1ae7613cc90018
Rejane Raquel <rejane.lima87@gmail.com>
2022-03-14 15:40:35 -0300

    Exercicio de fixaçao I 5.1

---
+++ 1fundamentos/bloco-05-java-script-dom-eventos-e-web-storage/dia-01-dom-e-seletores/script1.js
@@ -0,0 +1,5 @@
+//Exercícios fixação vídeio getElementById
+document.getElementById("page-title").innerText = "Breaking Bad";
+paragraph.style.color = "red";
+document.getElementById("second-paragraph").innerText="Say my Name";
+document.getElementById("subtitle").innerText="Heinsenberg"
