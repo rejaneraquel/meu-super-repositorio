d7a181a667c94abd539e505b04e0caa24129fad5
Rejane Raquel <rejane.lima87@gmail.com>
2022-03-16 12:02:49 -0300

    Adiciona fixaçao I

--- 1fundamentos/bloco-05-java-script-dom-eventos-e-web-storage/dia-01-dom-e-seletores/script2.js
+++ 1fundamentos/bloco-05-java-script-dom-eventos-e-web-storage/dia-01-dom-e-seletores/script2.js
@@ -1,4 +1,3 @@
-document.getElementsByClassName("paragrafos")[1].style.color = "orange" 
+document.getElementsByClassName("paragrafos")[1].style.color = "orange";
 
-
-
+document.getElementsByTagName("h4")[0].style.color = "yellowgreen"; //Prestar atenção no índice que começa no zero!
