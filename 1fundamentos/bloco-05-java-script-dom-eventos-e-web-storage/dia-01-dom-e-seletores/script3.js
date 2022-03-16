6ecd931b38a9258c19176918469ed93b2ce3fd37
Rejane Raquel <rejane.lima87@gmail.com>
2022-03-16 16:10:44 -0300

    Adiciona exercicios de fixaçao completos

---
+++ 1fundamentos/bloco-05-java-script-dom-eventos-e-web-storage/dia-01-dom-e-seletores/script3.js
@@ -0,0 +1,5 @@
+//exercícios fixação Query Selector (all)
+document.querySelector("h2").innerText = "Breaking Bad";
+paragraph.style.color = "red";
+document.querySelector("#second-paragraph").innerText="Say my Name";
+document.querySelector("#subtitle").innerText="Heinsenberg"
