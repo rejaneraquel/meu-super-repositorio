fccb07576f5af894b7e25e01e2f3eccd7deb885c
Rejane Raquel <rejane.lima87@gmail.com>
2022-04-10 13:44:35 -0300

    Atualiza Exercícios

---
+++ 1fundamentos/bloco-07-introducao-a-javascript-es6-e-testes-unitarios/dia-01-javascript-es6-let-const-arrow-functions-e-template-literal/exercicios-de-fixacao/script2.js
@@ -0,0 +1,25 @@
+if (true) {
+    // inicio do escopo do if
+    var userAge = '20';
+    console.log(userAge); // 20
+    // fim do escopo do if
+  }
+  console.log(userAge); // 20
+
+//let
+if (true) {
+    // inicio do escopo do if
+    let userAge = '20';
+    console.log(userAge); // 20
+    // fim do escopo do if
+  }
+  console.log(userAge); // não roda;
+
+//const
+if (true) {
+    // inicio do escopo do if
+    const userAge = '20';
+    console.log(userAge); // 20
+    // fim do escopo do if
+  }
+  console.log(userAge); // não roda;
---
+++ 1fundamentos/bloco-07-introducao-a-javascript-es6-e-testes-unitarios/dia-01-javascript-es6-let-const-arrow-functions-e-template-literal/exercicios-de-fixacao/script3.js
@@ -0,0 +1,12 @@
+var userName = 'Isabella';
+var userName = 'Lucas';
+console.log(userName); // Resultado: Lucas
+
+let userName = 'Isabella';
+let userName = 'Lucas';
+console.log(userName); // Resultado: erro
+
+const userName = 'Isabella';
+const userName = 'Lucas';
+console.log(userName); // Resultado: erro
+
