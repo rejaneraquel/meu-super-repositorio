d53e3ac09b40495003abeb169b24d632f40c50e5
Rejane Raquel <rejane.lima87@gmail.com>
2022-04-30 20:49:44 -0300

    Exercício 6 resolvido

--- 1fundamentos/bloco-8-higher-order-functions-do-javascript-es6/dia-2-javascript-es6-higher-order-functions-foreach-find-some-every-sort/exercise5.js
+++ 1fundamentos/bloco-8-higher-order-functions-do-javascript-es6/dia-2-javascript-es6-higher-order-functions-foreach-find-some-every-sort/exercise5.js
@@ -61,7 +61,7 @@ const books = [{
 ];
 
 // 5 - Faça uma função que retorne true , se todas as pessoas autoras nasceram no século XX, ou false , caso contrário.
-const expectedResult = false;
+const book = false;
 
 function everyoneWasBornOnSecXX() {
   return books.every((book) => (
