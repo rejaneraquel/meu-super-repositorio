8c11b7c5cbb50298e22de43ea8ba103b819d47c0
Rejane Raquel <rejane.lima87@gmail.com>
2022-05-19 20:35:27 -0300

    Adiciona exercício 4

--- 2front-end/bloco-9-javascript-e-testes-assincronos/dia-1-javascript-assincrono-e-callbacks/script3.js
+++ 2front-end/bloco-9-javascript-e-testes-assincronos/dia-1-javascript-assincrono-e-callbacks/script3.js
@@ -1,14 +1,14 @@
 const getPlanet = () => {
     const mars = {
-      name: "Mars",
-      distanceFromSun: {
-        value: 227900000,
-        measurementUnit: "kilometers",
-      },
+        name: "Mars",
+        distanceFromSun: {
+            value: 227900000,
+            measurementUnit: "kilometers",
+        },
     };
     setTimeout(() => console.log("Returned planet: ", mars), 4000);
-  };
-  
-  getPlanet(); // imprime Marte depois de 4 segundos
+};
 
-  //Adicionei a função setTimeout na linha 9 e o tempo solicitado;
+getPlanet(); // imprime Marte depois de 4 segundos
+
+//Adicionei a função setTimeout na linha 9 e o tempo solicitado;
