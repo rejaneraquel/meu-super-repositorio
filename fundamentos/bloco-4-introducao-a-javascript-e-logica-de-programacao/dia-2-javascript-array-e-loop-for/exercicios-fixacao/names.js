efcce2de086330b10fc194903a8864319cb3d9e5
Rejane Raquel <rejane.lima87@gmail.com>
2022-08-25 17:12:19 -0300

    Adiciona exercicio fixação for/of names

---
+++ fundamentos/bloco-4-introducao-a-javascript-e-logica-de-programacao/dia-2-javascript-array-e-loop-for/exercicios-fixacao/names.js
@@ -0,0 +1,14 @@
+// utilize for/of para imprimir os elementos da lista names com o console.log
+let names = ['João', 'Maria', 'Antônio', 'Margarida'];
+for (let letter of names) {
+    console.log(letter);
+}
+
+//No exemplo acima como é um array ele mostrou separadamente cada variável do array.
+
+// Ja abaixo como é uma variáve simples, foi soletrado;
+
+let word = 'Hello';
+for (let letter of word) {
+    console.log(letter);
+}
