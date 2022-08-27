811b5978be765b16c775daaf0ac475d60edac622
Rejane Raquel <rejane.lima87@gmail.com>
2022-08-27 18:38:39 -0300

    Exercícios Práticos - Numbers.js - 3,4 e 5

--- fundamentos/bloco-4-introducao-a-javascript-e-logica-de-programacao/dia-2-javascript-array-e-loop-for/exercicios-praticos/arrayNumbers.js
+++ fundamentos/bloco-4-introducao-a-javascript-e-logica-de-programacao/dia-2-javascript-array-e-loop-for/exercicios-praticos/arrayNumbers.js
@@ -10,3 +10,26 @@ for (let i = 0; i < numbers.length; i += 1) {
 result += numbers[i];
 }
 console.log(result);
+
+// 3. Calcule a média aritmética dos valores contidos no array
+let media = result/numbers.length;
+console.log(media);
+
+// 4. Caso a média for maior que 20 imprima: "valor maior que 20", Caso não, imprima (valor menor ou igual a 20)
+if (media > 20) {
+    console.log("Valor maior que 20")
+} else {
+    console.log("Valor menor ou igual a 20")
+};
+
+// Utilizando o for, descubra qual o maior valor contida no array e imprima-o;
+// Nesse caso como ja declaramos a variavel como o index[0], iremos inicar a conta do index[1];
+// Vou redeclarar um novo array, para não confundir o array acima;
+let numbers2 = [5, 9, 3, 19, 70, 8, 100, 2, 35, 27];
+let maiorNumero = numbers[0];
+for (let i = 1; i < numbers2.length; i +=1) {
+    if (numbers2[i] > maiorNumero) {
+        maiorNumero = numbers2[i]
+}
+}
+console.log(maiorNumero)
