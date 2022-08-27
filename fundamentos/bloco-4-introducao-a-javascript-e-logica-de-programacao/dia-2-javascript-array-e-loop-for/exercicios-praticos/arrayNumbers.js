0138d162cd53faade7f28cd99d352c69543da25b
Rejane Raquel <rejane.lima87@gmail.com>
2022-08-27 19:05:31 -0300

    Exercícios Práticos - Numbers.js - 6 e 7

--- fundamentos/bloco-4-introducao-a-javascript-e-logica-de-programacao/dia-2-javascript-array-e-loop-for/exercicios-praticos/arrayNumbers.js
+++ fundamentos/bloco-4-introducao-a-javascript-e-logica-de-programacao/dia-2-javascript-array-e-loop-for/exercicios-praticos/arrayNumbers.js
@@ -1,18 +1,18 @@
 let numbers = [5, 9, 3, 19, 70, 8, 100, 2, 35, 27];
 // 1. Percorra o array imprimindo os valores nele contidos com a função console.log.
 for (let i = 0; i < numbers.length; i += 1) {
- console.log(numbers[i]);
+    console.log(numbers[i]);
 }
 
 // 2. Some todos oa valores contidos no array e imrpima o resultado
 let result = 0;
 for (let i = 0; i < numbers.length; i += 1) {
-result += numbers[i];
+    result += numbers[i];
 }
 console.log(result);
 
 // 3. Calcule a média aritmética dos valores contidos no array
-let media = result/numbers.length;
+let media = result / numbers.length;
 console.log(media);
 
 // 4. Caso a média for maior que 20 imprima: "valor maior que 20", Caso não, imprima (valor menor ou igual a 20)
@@ -22,14 +22,34 @@ if (media > 20) {
     console.log("Valor menor ou igual a 20")
 };
 
-// Utilizando o for, descubra qual o maior valor contida no array e imprima-o;
+// 5. Utilizando o for, descubra qual o maior valor contida no array e imprima-o;
 // Nesse caso como ja declaramos a variavel como o index[0], iremos inicar a conta do index[1];
 // Vou redeclarar um novo array, para não confundir o array acima;
-let numbers2 = [5, 9, 3, 19, 70, 8, 100, 2, 35, 27];
+//let numbers2 = [5, 9, 3, 19, 70, 8, 100, 2, 35, 27];
 let maiorNumero = numbers[0];
-for (let i = 1; i < numbers2.length; i +=1) {
-    if (numbers2[i] > maiorNumero) {
-        maiorNumero = numbers2[i]
-}
+for (let i = 1; i < numbers.length; i += 1) {
+    if (numbers[i] > maiorNumero) {
+        maiorNumero = numbers[i]
+    }
 }
 console.log(maiorNumero)
+
+// 6. Descubra quantos valores ímpares existem no array e imprima o resultado. Caso não exista imprima "Nenhum valor impar encontrado"
+let numerosImpares = 0;
+for (let i = 0; i < numbers.length; i += 1) {
+    if (numbers[i] % 2 !== 0) {
+        numerosImpares += 1
+    }
+} if (numerosImpares === 0) {
+    console.log('nenhum valor ímpar encontrado')
+}
+ else { console.log(numerosImpares) }  
+
+// 7. utilizand o for, descubra qual o menor valor contido no array e imprima-o 
+let menorNumero = numbers[0];
+for (let i = 1; i < numbers.length; i += 1) {
+    if (numbers[i] < menorNumero) {
+        menorNumero = numbers[i]
+    }
+}
+console.log(menorNumero)
