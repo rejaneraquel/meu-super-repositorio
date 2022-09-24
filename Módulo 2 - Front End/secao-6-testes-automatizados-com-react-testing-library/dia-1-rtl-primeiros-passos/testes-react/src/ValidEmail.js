df422ba88ff243a4d55751a65e8cbb40437edfa4
Rejane Raquel <rejane.lima87@gmail.com>
2022-09-24 19:03:46 -0300

    Adiciona exercício de fixação 1

--- Módulo 2 - Front End/secao-6-testes-automatizados-com-react-testing-library/dia-1-rtl-primeiros-passos/testes-react/src/ValidEmail.js
+++ Módulo 2 - Front End/secao-6-testes-automatizados-com-react-testing-library/dia-1-rtl-primeiros-passos/testes-react/src/ValidEmail.js
@@ -13,13 +13,21 @@ const ValidEmail = (props) => {
   return (
     <div>
       <h2 data-testid="id-email-user">{`Valor: ${email}`}</h2>
-      <h3>{(verifyEmail(email) ? 'Email Válido' : 'Email Inválido')}</h3>
+         {
+             email && (
+                 <h3 data-testid="id-is-email-valid" >
+               {(verifyEmail(email) ? 'Email Válido' : 'Email Inválido')}
+             </h3>
+           )
+        }
     </div>
   );
 };
 
 ValidEmail.propTypes = {
-  email: PropTypes.string.isRequired,
+    email: PropTypes.string.isRequired,
 };
 
 export default ValidEmail;
+/* Adicionamos (linha 16 a 22) uma renderização condicional com o operador lógico '&&' para que
+    esse elemento <h3> só exista quando a variável email tiver algum valor verdadeiro.*/
