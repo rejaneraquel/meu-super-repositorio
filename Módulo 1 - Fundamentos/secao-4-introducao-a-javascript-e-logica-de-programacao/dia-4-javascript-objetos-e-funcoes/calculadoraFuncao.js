1d3ca7b4c072a1a6dfad310ff87087e6db7783dd
Rejane Raquel <rejane.lima87@gmail.com>
2023-01-04 22:00:24 -0300

    exercicios de fixação funções

---
+++ Módulo 1 - Fundamentos/secao-4-introducao-a-javascript-e-logica-de-programacao/dia-4-javascript-objetos-e-funcoes/calculadoraFuncao.js
@@ -0,0 +1,39 @@
+/* Faça cinco programas, um para cada operação aritmética básica. Seu programa deve ter duas constantes, a e b, definidas no começo com os valores que serão operados. Faça programas para:
+
+Adição (a + b)
+Subtração (a - b)
+Multiplicação (a * b)
+Divisão (a / b)
+Módulo (a % b) */
+
+function adicao (a, b) {
+    soma = a + b;
+    return soma;
+}
+console.log(adicao(121, 211))
+
+function subtracao (a, b) {
+    subtrair = a - b;
+    return subtrair;
+}
+console.log(subtracao(121, 211))
+
+function multiplicacao (a, b) {
+    vezes = a * b;
+    return vezes; 
+
+}
+console.log(multiplicacao(121, 211))
+
+function divisao (a, b) {
+    divide = a / b;
+    return divide; 
+
+}
+console.log(divisao(121, 211))
+
+function modulo (a, b) {
+    modular = a % b;
+    return modular;
+} 
+console.log(modulo(121, 211))
