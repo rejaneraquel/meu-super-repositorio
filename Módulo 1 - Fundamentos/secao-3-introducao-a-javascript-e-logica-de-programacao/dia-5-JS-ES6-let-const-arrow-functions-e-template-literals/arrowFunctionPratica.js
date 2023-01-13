a8c8422ef2e4af734ab6fdd782b220d94b0ff440
Rejane Raquel <rejane.lima87@gmail.com>
2023-01-13 19:41:00 -0300

    Adiciona exercícios Hello Arrow Functions

---
+++ Módulo 1 - Fundamentos/secao-3-introducao-a-javascript-e-logica-de-programacao/dia-5-JS-ES6-let-const-arrow-functions-e-template-literals/arrowFunctionPratica.js
@@ -0,0 +1,6 @@
+/* function numeroAleatorio() {
+    return Math.random()
+  } */
+
+  const numeroAleatorio = () => Math.random() 
+  console.log(numeroAleatorio());
---
+++ Módulo 1 - Fundamentos/secao-3-introducao-a-javascript-e-logica-de-programacao/dia-5-JS-ES6-let-const-arrow-functions-e-template-literals/helloArrowFunctions.js
@@ -0,0 +1,12 @@
+/* function hello(nome) {
+    return `Olá, ${nome}!`
+  }
+  let nome = 'Ivan';
+  console.log(hello(nome)); */
+
+  /* Transforme a função hello em uma arrow function; */
+  
+  const hello = (nome) => `Olá, ${nome}!`
+  
+  let nome = 'Ivan';
+  console.log(hello(nome));
