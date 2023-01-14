e94f8c204d3327af2e4d97bbb199065cc729743c
Rejane Raquel <rejane.lima87@gmail.com>
2023-01-14 16:41:32 -0300

    Adiciona práticas 1

---
+++ Módulo 1 - Fundamentos/secao-3-introducao-a-javascript-e-logica-de-programacao/dia-5-JS-ES6-let-const-arrow-functions-e-template-literals/nomeCompletoArrowFunction.js
@@ -0,0 +1,14 @@
+/* Transforme a função nomeCompleto em uma arrow function;
+
+function nomeCompleto(nome, sobrenome) {
+    return `${nome} ${sobrenome}`
+  }
+  let nome = 'Ivan';
+  let sobrenome = 'Pires';
+  console.log(nomeCompleto(nome, sobrenome)); */
+  const nomeCompleto = (nome, sobrenome) => {
+    return `${nome} ${sobrenome}`
+  }
+  let nome = 'Ivan';
+  let sobrenome = 'Pires';
+  console.log(nomeCompleto(nome, sobrenome));
