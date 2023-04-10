d79d78e0cf653d6ad735233190f1563efd659798
Rejane Raquel <rejane.lima87@gmail.com>
2023-04-10 13:45:07 -0300

    Não me recordo

--- Módulo 2 - Front End/secao-7-gerenciamento-de-estado-com-redux/frontend-dia-01-introducao-ao-redux/2-exercicio-do-dia-7.1/2-exercicio-do-dia-7.1/main.js
+++ Módulo 2 - Front End/secao-7-gerenciamento-de-estado-com-redux/frontend-dia-01-introducao-ao-redux/2-exercicio-do-dia-7.1/2-exercicio-do-dia-7.1/main.js
@@ -1,30 +1,23 @@
 const themeButton = document.getElementById('toggle-theme');
 const statusButton = document.getElementById
   ('toggle-status');
-const OFFLINE = 'OFFLINE';
-const ONLINE = 'ONLINE;'
-const THEME_DARK = 'THEME_DARK';
-const THEME_LIGHT = 'THEME_LIGHT';
+
+const CHANGE_THEME = 'CHANGE_THEME';
+const CHANGE_STATUS = 'CHANGE_STATUS';
 
 const INITIAL_STATE = {}
 const reducer = (state = INITIAL_STATE, action) => {
   switch (action.type) {
-   case THEME_DARK: 
-   return { 
-    // fazer lógica;
-   };
-   case THEME_LIGHT: 
-   return { 
-    // fazer lógica;
-   };
-   case ONLINE: 
-   return { 
-    // fazer lógica;
-   };
-   case OFFLINE: 
-   return { 
-    // fazer lógica;
-   };
+    case CHANGE_THEME:
+      return {
+        ...state,
+        theme: state.theme === 'dark' ? 'light' : 'dark'
+      };
+    case CHANGE_STATUS:
+      return {
+        ...state,
+        status: state.status === 'offline' ? 'online' : 'offline'
+      };
 
     default:
       return state
@@ -35,9 +28,11 @@ const store = Redux.createStore(reducer)
 
 themeButton.addEventListener('click', () => {
   // coloque o código aqui.
-  console.log(store.dispatch({ type: THEME_DARK, teste: 'ok' }))
-});
+  const { state } = store.getState();
+  console.log(store.dispatch({ type: CHANGE_THEME }))
+ });
 
 statusButton.addEventListener('click', () => {
   // coloque o código aqui.
-});
+  console.log(store.dispatch({ type: CHANGE_STATUS, teste: 'OFFLINE' }))
+  });
