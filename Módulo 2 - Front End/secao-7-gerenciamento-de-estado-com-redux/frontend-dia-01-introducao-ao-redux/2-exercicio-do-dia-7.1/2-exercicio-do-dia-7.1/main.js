957c242bbc349eafab2581e16535b4fa9588cf9a
Rejane Raquel <rejane.lima87@gmail.com>
2023-04-10 12:30:21 -0300

    Adicionando o reducer, a store,a base do switch e a base do subscribe

--- Módulo 2 - Front End/secao-7-gerenciamento-de-estado-com-redux/frontend-dia-01-introducao-ao-redux/2-exercicio-do-dia-7.1/2-exercicio-do-dia-7.1/main.js
+++ Módulo 2 - Front End/secao-7-gerenciamento-de-estado-com-redux/frontend-dia-01-introducao-ao-redux/2-exercicio-do-dia-7.1/2-exercicio-do-dia-7.1/main.js
@@ -1,8 +1,41 @@
 const themeButton = document.getElementById('toggle-theme');
-const statusButton = document.getElementById('toggle-status');
+const statusButton = document.getElementById
+  ('toggle-status');
+const OFFLINE = 'OFFLINE';
+const ONLINE = 'ONLINE;'
+const THEME_DARK = 'THEME_DARK';
+const THEME_LIGHT = 'THEME_LIGHT';
+
+const INITIAL_STATE = {}
+const reducer = (state = INITIAL_STATE, action) => {
+  switch (action.type) {
+   case THEME_DARK: 
+   return { 
+    // fazer lógica;
+   };
+   case THEME_LIGHT: 
+   return { 
+    // fazer lógica;
+   };
+   case ONLINE: 
+   return { 
+    // fazer lógica;
+   };
+   case OFFLINE: 
+   return { 
+    // fazer lógica;
+   };
+
+    default:
+      return state
+  }
+};
+const store = Redux.createStore(reducer)
+// console.log(reducer);
 
 themeButton.addEventListener('click', () => {
   // coloque o código aqui.
+  console.log(store.dispatch({ type: THEME_DARK, teste: 'ok' }))
 });
 
 statusButton.addEventListener('click', () => {
