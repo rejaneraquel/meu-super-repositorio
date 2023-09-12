7a25409f05d301d51944c086badd98006c3ee107
Rejane Raquel <98369885+rejaneraquel@users.noreply.github.com>
2023-09-12 13:15:08 +0000

    Adiciona readline sobrenome no index do arquivo hello-world

--- Modulo3-BackEnd/secao-4-introducao-ao-desenvolvimento-web-com-node/dia-1-runtime-assincrono/exercicio-pratica/hello-world/index.js
+++ Modulo3-BackEnd/secao-4-introducao-ao-desenvolvimento-web-com-node/dia-1-runtime-assincrono/exercicio-pratica/hello-world/index.js
@@ -2,5 +2,6 @@ const readline = require('readline-sync');
 
 const name = readline.question('Qual seu nome? ');
 const age = readline.questionInt('Qual sua idade? ');
+const lastName = readline.question('Qual seu sobrenome? ')
 
-console.log(`Hello, ${name}! You are ${age} years old!`);
+console.log(`Hello, ${name} ${lastName}! You are ${age} years old!`);
