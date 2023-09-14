0a1ebe84444f44d3c24af340b276eb9a41f5010b
Rejane Raquel <98369885+rejaneraquel@users.noreply.github.com>
2023-09-14 16:22:16 +0000

    corrigindo arquivo bmi

--- Modulo3-BackEnd/secao-4-introducao-ao-desenvolvimento-web-com-node/dia-1-runtime-assincrono/pratico/bmi.js
+++ Modulo3-BackEnd/secao-4-introducao-ao-desenvolvimento-web-com-node/dia-1-runtime-assincrono/pratico/bmi.js
@@ -1,5 +1,24 @@
-const imc = (peso, altura) => {
-    return peso / (altura * altura);
+const readline = require('readline-sync');
+
+const pesoKg = 80;
+const alturaCm = 178;
+
+function handleIMC(peso, altura) {
+    console.log(`Peso: ${peso} Kg, Altura: ${altura} cm`)
+    
+    const alturaMetros = altura / 100;
+    const alturaQuadrado = alturaMetros ** 2;
+
+    const bmi = peso/alturaQuadrado;
+
+    return bmi
+    
 }
 
-console.log(imc(80, 1.80));
+function main() {
+    const bmi = handleIMC(pesoKg, alturaCm);
+    
+    console.log(`IMC: ${bmi.toFixed(2)}`)
+}
+
+main();
