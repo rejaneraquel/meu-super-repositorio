33503d5a5e3281d278bda0c32f8372edee2edace
Rejane Lima <rejane.lima87@gmail.com>
2023-09-14 13:46:30 -0300

    Torna interativo com a leitura/resposta pela linha de comando

--- Modulo3-BackEnd/secao-4-introducao-ao-desenvolvimento-web-com-node/dia-1-runtime-assincrono/pratico/bmi.js
+++ Modulo3-BackEnd/secao-4-introducao-ao-desenvolvimento-web-com-node/dia-1-runtime-assincrono/pratico/bmi.js
@@ -1,22 +1,22 @@
 const readline = require('readline-sync');
 
-const pesoKg = 80;
-const alturaCm = 178;
 
 function handleIMC(peso, altura) {
     console.log(`Peso: ${peso} Kg, Altura: ${altura} cm`)
     
     const alturaMetros = altura / 100;
     const alturaQuadrado = alturaMetros ** 2;
-
+    
     const bmi = peso/alturaQuadrado;
-
+    
     return bmi
     
 }
 
 function main() {
-    const bmi = handleIMC(pesoKg, alturaCm);
+    const peso = readline.questionInt(`Qual seu peso(kg)? `);
+    const altura = readline.questionInt(`Qual sua altura (cm)? `);
+    const bmi = handleIMC(peso, altura);
     
     console.log(`IMC: ${bmi.toFixed(2)}`)
 }
