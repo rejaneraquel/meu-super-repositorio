31b907a106f7fc2dcdfc52ce6e9a4d9446fc9407
Rejane Lima <rejane.lima87@gmail.com>
2023-09-19 16:01:41 -0300

    alterando para port: 3005

--- Modulo3-BackEnd/secao-4-introducao-ao-desenvolvimento-web-com-node/dia-2-API-rest-com-express/exercicios-aprendizagem/soccer-team-manager/src/app.js
+++ Modulo3-BackEnd/secao-4-introducao-ao-desenvolvimento-web-com-node/dia-2-API-rest-com-express/exercicios-aprendizagem/soccer-team-manager/src/app.js
@@ -2,4 +2,21 @@ const express = require('express');
 
 const app = express();
 
+const teams = [
+  {
+    id: 1,
+    name: 'São Paulo Futebol Clube',
+    initials: 'SPF',
+  },
+  {
+    id: 2,
+    name: 'Clube Atlético Mineiro',
+    initials: 'CAM',
+  },
+];
+
+app.get('/', (req, res) => res.status(200).json({ message: 'Olá Mundo!' }));
+
+app.get('/teams', (req, res) => res.status(200).json({ teams }));
+
 module.exports = app;
