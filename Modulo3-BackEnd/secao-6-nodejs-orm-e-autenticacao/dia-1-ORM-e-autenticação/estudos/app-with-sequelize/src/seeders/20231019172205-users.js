d167750c2f2656d1a17633fd37b583f83e5e2fa5
Rejane Lima <rejane.lima87@gmail.com>
2023-10-19 14:59:12 -0300

    Adiciona seeds

---
+++ Modulo3-BackEnd/secao-6-nodejs-orm-e-autenticacao/dia-1-ORM-e-autenticação/estudos/app-with-sequelize/src/seeders/20231019172205-users.js
@@ -0,0 +1,27 @@
+// src/seeders/[timestamp]-users.js
+// Para criar uma seed: npx sequelize seed:generate --name users
+// Up: env $(cat .env) npx sequelize db:seed:all
+// Down: env $(cat .env) npx sequelize db:seed:undo:all
+
+'use strict';
+
+module.exports = {
+  up: async (queryInterface, Sequelize) => queryInterface.bulkInsert('Users',
+    [
+      {
+        fullName: 'Leonardo',
+        email: 'leo@test.com',
+        // usamos a função CURRENT_TIMESTAMP do SQL para salvar a data e hora atual nos campos `createdAt` e `updatedAt`
+        createdAt: Sequelize.literal('CURRENT_TIMESTAMP'),
+        updatedAt: Sequelize.literal('CURRENT_TIMESTAMP'),
+      },
+      {
+        fullName: 'JEduardo',
+        email: 'edu@test.com',
+        createdAt: Sequelize.literal('CURRENT_TIMESTAMP'),
+        updatedAt: Sequelize.literal('CURRENT_TIMESTAMP'),
+      },
+    ], {}),
+
+  down: async (queryInterface) => queryInterface.bulkDelete('Users', null, {}),
+};
