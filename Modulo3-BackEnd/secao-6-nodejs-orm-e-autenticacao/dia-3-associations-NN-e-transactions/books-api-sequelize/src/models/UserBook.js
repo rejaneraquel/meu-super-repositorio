a798ed543e5fa40904d037f593e5bcc25996af78
Rejane Lima <rejane.lima87@gmail.com>
2023-10-22 14:05:24 -0300

    Criando models

---
+++ Modulo3-BackEnd/secao-6-nodejs-orm-e-autenticacao/dia-3-associations-NN-e-transactions/books-api-sequelize/src/models/UserBook.js
@@ -0,0 +1,15 @@
+// src/models/UserBook.js
+
+module.exports = (sequelize, _DataTypes) => {
+  const UserBook = sequelize.define(
+    'UserBook',
+    {},
+    {
+      timestamps: false,
+      underscored: true,
+      tableName: 'users_books',
+    },
+  );
+
+  return UserBook;
+};
